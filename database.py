9ef206943238c75018aa41764c2b72bceaf442c5
matitruco <anovazzimatias182@gmail.com>
2025-08-30 14:47:46 -0300

    correccion en enlace a db

--- database.py
+++ database.py
@@ -1,10 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:holamati@mysql.railway.internal:3306/railway"
+# Obtener las variables de entorno proporcionadas por Railway
+user = os.environ["MYSQLUSER"]                   # Ej: 'root' o 'railway'
+password = os.environ["MYSQL_ROOT_PASSWORD"]    # Contraseña generada por Railway
+host = os.environ["RAILWAY_PRIVATE_DOMAIN"]     # Host interno de la DB
+port = os.environ.get("MYSQLPORT", 3306)        # Puerto de la DB
+database = os.environ["MYSQLDATABASE"]          # Nombre de la DB
 
+# Construir la URL de conexión
+DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
-
+# Crear engine y sesión
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
