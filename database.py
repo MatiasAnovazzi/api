d4151b8e69be4182ed4c0c666be8d9e81b36aa39
matitruco <anovazzimatias182@gmail.com>
2025-08-30 15:45:36 -0300

    qqqqqqqqqq

--- database.py
+++ database.py
@@ -2,10 +2,22 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+
+
+
 DATABASE_URL = (
     f"mysql+pymysql://{os.getenv('MYSQLUSER')}:{os.getenv('MYSQLPASSWORD')}"
     f"@{os.getenv('MYSQLHOST')}:{os.getenv('MYSQLPORT')}/{os.getenv('MYSQL_DATABASE')}"
 )
+
+print("MYSQLUSER:", os.getenv("MYSQLUSER"))
+print("MYSQLPASSWORD:", os.getenv("MYSQLPASSWORD"))
+print("MYSQLHOST:", os.getenv("MYSQLHOST"))
+print("MYSQLPORT:", os.getenv("MYSQLPORT"))
+print("MYSQLDATABASE:", os.getenv("MYSQLDATABASE"))
+print("DATABASE_URL:", DATABASE_URL)
+
+
 # Crear engine y sesión
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
