e3eeb180f810d8e36c18e0687c4e94765233811a
matitruco <anovazzimatias182@gmail.com>
2025-08-30 15:25:58 -0300

    fffffffff

--- database.py
+++ database.py
@@ -2,16 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Obtener las variables de entorno proporcionadas por Railway
-user = os.environ["MYSQLUSER"]                   # Ej: 'root' o 'railway'
-password = os.environ["MYSQLPASSWORD"]    # Contraseña generada por Railway
-host = os.environ["MYSQLHOST"]                  # Host interno de la DB
-port = os.environ.get("MYSQLPORT", 3306)        # Puerto de la DB
-database = os.environ["MYSQL_DATABASE"]          # Nombre de la DB
-
-# Construir la URL de conexión
-DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
-
+DATABASE_URL = (
+    f"mysql+pymysql://{os.getenv('MYSQLUSER')}:{os.getenv('MYSQLPASSWORD')}"
+    f"@{os.getenv('MYSQLHOST')}:{os.getenv('MYSQLPORT')}/{os.getenv('MYSQLDATABASE')}"
+)
 # Crear engine y sesión
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
