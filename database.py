8f1754fc7925d1e5c99110abc241d3627024c155
matitruco <anovazzimatias182@gmail.com>
2025-08-31 13:03:19 -0300

    pip

--- database.py
+++ database.py
@@ -4,6 +4,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print(DATABASE_URL)
+
+if DATABASE_URL and DATABASE_URL.startswith("mysql://"):
+    DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
+
+print(DATABASE_URL)
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
