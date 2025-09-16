25e260028ddcc5e9d88b5d5e527269e95940d2b3
chu <chu@chu.com.ar>
2025-09-16 16:59:38 -0300

    asasas

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 from routers import usuarios, turnos
 from database import Base, engine
 import uvicorn
@@ -10,9 +9,6 @@ import uvicorn
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-app.add_middleware(ProxyHeadersMiddleware, trusted_hosts="*")
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
